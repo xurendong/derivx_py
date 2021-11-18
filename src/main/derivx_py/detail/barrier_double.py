eab944da618ee9b003affd9fb82df986cea35f76
xurendong <xrdlyj@163.com>
2021-11-18 14:05:39 +0800

    完善 Barrier Double 结构及其示例

--- src/main/derivx_py/detail/barrier_double.py
+++ src/main/derivx_py/detail/barrier_double.py
@@ -26,17 +26,31 @@ import utility
 
 class Barrier_Double(object):
     def __init__(self):
+        self.rand_rows = 0 # 随机数据行数 # InitRand
+        self.rand_cols = 0 # 随机数据列数 # InitRand
+        self.rand_seed = np.array([]) # 随机数据种子 # InitRand # 非负整数，有效位数不超逻辑处理器数量
+        
+        self.dual_smooth = True # 对偶平滑路径 # InitPath
+        self.runs_size = 0 # 模拟路径数量 # InitPath
+        self.runs_step = 0 # 价格变动步数 # InitPath
+        self.year_days = 0 # 年交易日数量 # InitPath
+        self.sigma = 0.0 # 波动率 # InitPath
+        self.risk_free_rate = 0.0 # 无风险利率 # InitPath
+        self.basis_rate = 0.0 # 股息或贴水 # InitPath
+        self.price_limit_ratio = 0.0 # 涨跌停限制幅度 # InitPath
+        self.price_limit_style = 0 # 涨跌停限制方式，0 不限制，1 超限部分移至下日，2 超限部分直接削掉 # InitPath
+        
         self.s = 0.0 # 标的价格
-        self.h = 0.0 # 障碍价格
-        self.k = 0.0 # 行权价格
-        self.x = 0.0 # 未触及障碍所需支付资金
-        self.v = 0.0 # 波动率
-        self.r = 0.0 # 无风险利率
-        self.q = 0.0 # 年化分红率
-        self.t = 0.0 # 年化到期期限
+        self.h_l = 0.0 # 障碍价格，低
+        self.h_h = 0.0 # 障碍价格，高
+        self.k_l = 0.0 # 行权价格，低
+        self.k_h = 0.0 # 行权价格，高
+        self.x = 0.0 # 敲出后需支付的资金
+        self.v = 0.0 # 波动率 # 双鲨未用
+        self.r = 0.0 # 无风险利率 # 双鲨未用
+        self.q = 0.0 # 年化分红率 # 双鲨未用
+        self.t = 0.0 # 年化到期期限 # 双鲨未用
         self.p = 0.0 # 参与率，未敲出情况下客户对收益的占比要求
-        self.is_call = True # 看涨看跌
-        self.is_knock = False # 是否已经敲入敲出
         self.is_kop_delay = False # 敲出后是立即还是延期支付资金
         self.barrier_type = 0 # 障碍类型
         
@@ -47,17 +61,29 @@ class Barrier_Double(object):
 
     def InitArgs(self, config): # config：dict
         try:
+            self.rand_rows = config["rand_rows"]
+            self.rand_cols = config["rand_cols"]
+            self.rand_seed = config["rand_seed"].copy() # copy
+            self.dual_smooth = config["dual_smooth"]
+            self.runs_size = config["runs_size"]
+            self.runs_step = config["runs_step"]
+            self.year_days = config["year_days"]
+            self.sigma = config["sigma"]
+            self.risk_free_rate = config["risk_free_rate"]
+            self.basis_rate = config["basis_rate"]
+            self.price_limit_ratio = config["price_limit_ratio"]
+            self.price_limit_style = config["price_limit_style"]
             self.s = config["s"]
-            self.h = config["h"]
-            self.k = config["k"]
+            self.h_l = config["h_l"]
+            self.h_h = config["h_h"]
+            self.k_l = config["k_l"]
+            self.k_h = config["k_h"]
             self.x = config["x"]
             self.v = config["v"]
             self.r = config["r"]
             self.q = config["q"]
             self.t = config["t"]
             self.p = config["p"]
-            self.is_call = config["is_call"]
-            self.is_knock = config["is_knock"]
             self.is_kop_delay = config["is_kop_delay"]
             self.barrier_type = config["barrier_type"]
             return 0
